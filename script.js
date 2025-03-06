d6c5f9f14a06a2f527f39c95edb730e363d0f232
ChrisRoland <chrisebuberoland@gmail.com>
2025-03-06 15:15:32 +0100

    js added

---
+++ script.js
@@ -0,0 +1,13 @@
+const clicked = [];
+const magicKey = "ArrowUpArrowUpArrowLeftArrowUpArrowRightArrowRightArrowDown";
+
+window.addEventListener("keyup", (e) => {
+  clicked.push(e.key);
+  clicked.splice(
+    -magicKey.length - 1,
+    clicked.length - magicKey.length
+  );
+  if (clicked.join("").includes(magicKey)) {
+    cornify_add();
+  }
+});
